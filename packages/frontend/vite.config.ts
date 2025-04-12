a480e744fcab85ff6b7f47a628410152f06b08e6
Ebooth <pauldejeandev@gmail.com>
2025-04-12 20:46:22 +0200

    fix

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import fs from "fs";
+// import fs from "fs";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
