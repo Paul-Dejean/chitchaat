b788a5ee6d0cc197dca2effb6a97927474f71bc2
Ebooth <pauldejeandev@gmail.com>
2025-04-14 23:30:14 +0200

    refacto

--- packages/frontend/src/ui-library/EmailInput/index.tsx
+++
@@ -1,5 +0,0 @@
-import { InputHTMLAttributes } from "react";
-
-export function EmailInput(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} type="email" placeholder="Email" className="p-2" />;
-}
